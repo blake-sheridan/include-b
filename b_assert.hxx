a62c7dded4b52e1fa5dba442034b60af3a303152
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-14 22:41:55 -0500

    Suppress errant clang warning

--- b_assert.hxx
+++ b_assert.hxx
@@ -1,5 +1,12 @@
 #ifndef b_assert
 
+// Allow either:
+//     b_assert(x)
+//     b_assert(x, "message")
+// Clang issues this warning, but both work just fine with my GCC.
+// I couldn't find a way to push this, even with `_Pragma`.
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
 
